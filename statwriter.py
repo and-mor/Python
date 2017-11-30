e13dd17cd36892b2b59db487f5f155b17bcee166
and-mor <34125665+and-mor@users.noreply.github.com>
2017-11-30 04:03:57 -0600

    Add files via upload

---
+++ statwriter.py
@@ -0,0 +1,12 @@
+import datetime
+import psutil
+time = ('{:%Y%m%d%H%M%S}'.format(datetime.datetime.now()))
+print (time)
+nsfilename = str(time) + ".txt"
+nsfile = open(nsfilename, "w+") 
+lget = list(psutil.win_service_iter())
+for name in lget:
+	sdetails = (list(psutil.win_service_get(str(name))) + sget.as_dict())
+	nsfile.write(sdetails)
+nsfile.close()
+print (nsfile)
